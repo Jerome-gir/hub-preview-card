392d4c188b83706cef0e238acbe3a67b54567fb8
Jerome <jerome.gir@gmail.com>
2024-09-13 23:21:15 +0200

    mobile version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config: Config = {
   content: [
@@ -8,12 +9,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        figtree: ["var(--font-figtree)", ...fontFamily.sans],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        yellow: "var(--yellow)",
+      },
+      dropShadow: {
+        "custom-black": "10px 5px 2px rgba(0, 0, 0, 1)", // Ombre noire
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
